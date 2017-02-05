ccc4d4c26eeca3d598f4c47423cd3cba9de3791f
Sabrina Peng <Sabrina Peng>
2017-02-05 00:40:30 -0600

    Worked on match.cpp

--- Sprint1/Sprint1/match.cpp
+++ Sprint1/Sprint1/match.cpp
@@ -1,16 +1,12 @@
-/*
 #include "match.h"
 
-Match(String teamOne, String teamTwo, String match, String verb) {
-    teamOneFile = teamOne;
-    teamTwoFile = teamTwo;
-    matchFile = match;
-    verbosity = verb;
+Match::Match() {
+    
 }
 
-void Match::readTeamFile() {
+void Match::readTeamOneFile(String teamFileName) {
     ifstream inFile;
-    inFile.open(teamOneFile, ios::in);
+    inFile.open(teamFileName, ios::in);
     if (!inFile) {
         cerr << "File 1 could not be opened" << endl;
         exit(EXIT_FAILURE);
@@ -18,42 +14,205 @@ void Match::readTeamFile() {
 
     String teamName;
     int teamSize;
-    inFile >> teamName >> teamSize;
-    Team teamOne(teamName, teamSize);
+    inFile.getline(teamName, 100);
+    inFile >> teamSize;
+    teamOne(teamName, teamSize);
 
     int playerID;
     String playerName;
-    inFile >> playerID >> playerName;
+    inFile >> playerID;
+    inFile.getline(playerName, 100);
     while(!inFile.eof()) {
-        inFile >> playerID >> playerName;
         Player p(playerID, playerName);
         teamOne.addPlayer(p);
+        inFile >> playerID;
+        inFile.getline(playerName, 100);
     }
 
     inFile.close();
+}
 
-    inFile.open(teamTwoFile, ios::in);
+void Match::readTeamTwoFile(String teamFileName) {
+    ifstream inFile;
+    inFile.open(teamFileName, ios::in);
     if (!inFile) {
         cerr << "File 2 could not be opened" << endl;
         exit(EXIT_FAILURE);
     }
 
+    String teamName;
+    int teamSize;
+    inFile.getline(teamName, 100);
+    inFile >> teamSize;
+    teamTwo(teamName, teamSize);
+
+    int playerID;
+    String playerName;
+    inFile >> playerID;
+    inFile.getline(playerName, 100);
+    while(!inFile.eof()) {
+        Player p(playerID, playerName);
+        teamTwo.addPlayer(p);
+        inFile >> playerID;
+        inFile.getline(playerName, 100);
+    }
+
     inFile.close();
 }
 
-void Match::readMatchFile() {
+void Match::readMatchFile(String matchFile) {
+    ifstream inFile;
+    inFile.open(matchFile, ios::in);
+    if (!inFile) {
+        cerr << "Match file could not be opened" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int totalTags;
+    inFile >> totalTags;
+
+    int tagger;
+    int tagged;
+    int tagTime;
+    int tagLocation;
+    inFile >> tagger >> tagged >> tagTime >> tagLocation;
+    while(!inFile.eof()) {
+        Tag t(tagger, tagged, tagLocation);
+        for (Player p: teamOne.getTeamPlayers()) {
+	    if (p.getID() == tagger) {
+		p.addTag(t);
+	    }
+	}
+        for (Player p: teamTwo.getTeamPlayers()) {
+	    if (p.getID() == tagger) {
+		p.addTag(t);
+	    }
+	}
+	inFile >> tagger >> tagged >> tagTime >> tagLocation;
+    }
+
+    inFile.close();
+}
+
+void Match::outputLowVerbosity() {
+    ofstream outFile("vlow.txt", ios::out);
+    if (!outFile) {
+        cerr << "Vlow file could not be created" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (teamOne.getTeamName() > teamTwo.getTeamName()) {
+        outFile << teamTwo.getTeamName() << ": " << teamTwo.calculateTeamScore() << " points" << endl;
+        outFile << teamOne.getTeamName() << ": " << teamOne.calculateTeamScore() << " points" << endl;
+    }
+    else {
+        outFile << teamOne.getTeamName() << ": " << teamOne.calculateTeamScore() << " points" << endl;
+        outFile << teamTwo.getTeamName() << ": " << teamTwo.calculateTeamScore() << " points" << endl;
+    }
+
+    if (teamOne.calculateTeamScore() > teamTwo.calculateTeamScore()){
+        outFile << "Overall Winners: " << teamOne.getTeamName() << endl;
+    }
+    else if (teamOne.calculateTeamScore() < teamTwo.calculateTeamScore()) {
+        outFile << "Overall Winners: " << teamTwo.getTeamName() << endl;
+    }
+    else {
+        outFile << "Overall Winners: Teams Tied" << endl;
+    }
+}
+
+void Match::outputMedVerbosity() {
+    ofstream outFile("vmed.txt", ios::out);
+    if (!outFile) {
+        cerr << "Vmed file could not be created" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (teamOne.getTeamName() > teamTwo.getTeamName()) {
+        outFile << teamTwo.getTeamName() << endl;
+        int mostTags = 0;
+        for (Player p: teamTwo.getTeamPlayers()) {
+            if (p.calculateNumTags() > mostTags) {
+                mostTags = p.calculateNumTags();
+            }
+        }
+        for (int i = mostTags; i > -1; i++) {
+            for (Player p: teamTwo.getTeamPlayers()) {
+                if (p.calculateNumTags() == mostTags) {
+                    for (Tag t: p.getPlayerTags()) {
+                        output << setw(10)<< p.getName() << " had a total of " << p.calculateNumTags() << "tags" << endl;
+                    }
+                    teamTwo.getTeamPlayers().pop_back(p);
+                }
+            }
+        }
+        outFile << endl;
+
+        outFile << teamOne.getTeamName() << endl;
+        mostTags = 0;
+        for (Player p: teamOne.getTeamPlayers()) {
+            if (p.calculateNumTags() > mostTags) {
+                mostTags = p.calculateNumTags();
+            }
+        }
+        for (int i = mostTags; i > -1; i++) {
+            for (Player p: teamOne.getTeamPlayers()) {
+                if (p.calculateNumTags() == mostTags) {
+                    for (Tag t: p.getPlayerTags()) {
+                        output << setw(10)<< p.getName() << " had a total of " << p.calculateNumTags() << "tags" << endl;
+                    }
+                    teamOne.getTeamPlayers().pop_back(p);
+                }
+            }
+        }
+        outFile << endl;
+    }
+    else {
+        outFile << teamOne.getTeamName() << endl;
+        int mostTags = 0;
+        for (Player p: teamOne.getTeamPlayers()) {
+            if (p.calculateNumTags() > mostTags) {
+                mostTags = p.calculateNumTags();
+            }
+        }
+        for (int i = mostTags; i > -1; i++) {
+            for (Player p: teamOne.getTeamPlayers()) {
+                if (p.calculateNumTags() == mostTags) {
+                    for (Tag t: p.getPlayerTags()) {
+                        output << setw(10)<< p.getName() << " had a total of " << p.calculateNumTags() << "tags" << endl;
+                    }
+                    teamOne.getTeamPlayers().pop_back(p);
+                }
+            }
+        }
+        outFile << endl;
+
+        outFile << teamTwo.getTeamName() << endl;
+        mostTags = 0;
+        for (Player p: teamTwo.getTeamPlayers()) {
+            if (p.calculateNumTags() > mostTags) {
+                mostTags = p.calculateNumTags();
+            }
+        }
+        for (int i = mostTags; i > -1; i++) {
+            for (Player p: teamTwo.getTeamPlayers()) {
+                if (p.calculateNumTags() == mostTags) {
+                    for (Tag t: p.getPlayerTags()) {
+                        output << setw(10)<< p.getName() << " had a total of " << p.calculateNumTags() << "tags" << endl;
+                    }
+                    teamTwo.getTeamPlayers().pop_back(p);
+                }
+            }
+        }
+        outFile << endl;
+    }
+
+    output << "Best score from " << teamOne.getTeamName() << ": ";
+
+    output << "Best score from " << teamTwo.getTeamName() << ": ";
+}
+
+void Match::outputHighVerbosity() {
 
 }
 
-void Match::printLowVerbosity(String vlow) {
-
-}
-
-void Match::printMedVerbosity(String vmed) {
-
-}
-
-void Match::printHighVerbosity(String vhigh) {
-
-}
-*/
--- Sprint1/Sprint1/match.h
+++ Sprint1/Sprint1/match.h
@@ -1,4 +1,3 @@
-/*
 #ifndef MATCH_H
 #define MATCH_H
 
@@ -6,27 +5,27 @@
 #include <cstring>
 #include <fstream>
 #include <cstdlib>
+#include <iomanip>
 #include "dsstring.h"
 #include "team.h"
 #include "player.h"
+#include "tag.h"
 
 //using namespace std;
 
 class Match {
     private:
-        String teamOneFile;
-        String teamTwoFile;
-        String matchFile;
-        String verbosity;
+        Team teamOne;
+        Team teamTwo;
 
     public:
         Match(String, String, String, String);
-        void readTeamFile();
+        void readTeamOneFile();
+        void readTeamTwoFile();
         void readMatchFile();
-        void printLowVerbosity(String);
-        void printMedVerbosity(String);
-        void printHighVerbosity(String);
+        void outputLowVerbosity();
+        void outputMedVerbosity();
+        void outputHighVerbosity();
 };
 
 #endif // MATCH_H
-*/
