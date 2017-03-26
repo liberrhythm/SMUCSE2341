63aa666eac5ebfb40c08a5b59b53dc55cb346f20
Sabrina Peng <shpeng@smu.edu>
2017-03-26 15:45:07 -0500

    Added catch code in main

--- Sprint4/Sprint4/Sprint4/main.cpp
+++ Sprint4/Sprint4/Sprint4/main.cpp
@@ -1,9 +1,41 @@
+/*
+Course Number:  CSE 2341
+Programmer:     Sabrina Peng
+Date:           2/5/17
+Program Number: Sprint2
+Purpose:        Reads in command line arguments to read files and write output to files for laser tag game implementation
+Instructor: 	Mark Fontenot
+TA:             Chris Henk, Kevin Queenan
+Sources Consulted: Stack Overflow, C++ How to Program by Deitel, Deitel
+*/
+
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
 #include <iostream>
+#include <fstream>
+#include <cstring>
+
+#include "DSString.h"
+#include "DSVector.h"
+#include "tag.h"
+#include "player.h"
+#include "team.h"
+#include "match.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    cout << "Hello World!" << endl;
+/*
+#define TEST true
+int runCatchTests(int argc, char* const argv[]) {
+    return Catch::Session().run(argc, argv);
+}
+int main(int argc, char* const argv[]) {
+    if(TEST)
+    {
+        return runCatchTests(argc, argv);
+    }
     return 0;
 }
+*/
